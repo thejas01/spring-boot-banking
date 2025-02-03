7045ba96ba6744e40a3afeceb1d38321944e3038
thejas01 <thejasgs19@gmail.com>
2025-02-03 10:30:41 +0530

    #2 all basic files are commited

--- src/main/java/com/thejas/banking/entity/User.java
+++ src/main/java/com/thejas/banking/entity/User.java
@@ -1,5 +1,97 @@
 package com.thejas.banking.entity;
 
 public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private String address;
+    private String dob;
+    private
+
+    public User() {
+    }
+
+    public User(int id, String username, String password, String email, String phone, String address, String dob) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.dob = dob;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getDob() {
+        return this.dob;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", dob='" + dob + '\'' +
+                '}';
+    }
     
 }
